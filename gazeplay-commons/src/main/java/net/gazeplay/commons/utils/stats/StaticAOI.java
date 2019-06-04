ae9ea7885608e61e26166c9768abcd4c62904c97
Wafaa <wafaa.el-husseini@grenoble-inp.org>
2019-06-04 12:16:37 +0200

    Added Missing Arabic Translations

--- gazeplay-commons/src/main/java/net/gazeplay/commons/utils/stats/StaticAOI.java
+++ gazeplay-commons/src/main/java/net/gazeplay/commons/utils/stats/StaticAOI.java
@@ -2,6 +2,19 @@ package net.gazeplay.commons.utils.stats;
 
 public class StaticAOI {
 
+    int totalFixationsCount;
+
+    public class staticAOIBox{
+        int x;
+        int y;
+        double width;
+        double height;
+
+        int countFixations;
+
+
+    }
+
 
 
 
